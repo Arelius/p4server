002374890e0ad7a8525ff204e86dfbb15ec221b1
Nicholas "Indy" Ray <Arelius@gmail.com>
2015-12-10 23:49:34 -0800

    Added s3 backup script

---
+++ backup_to_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BACKUP_SOURCE=/path/to/backup
+S3BUCKET=bucket-name
+ARCHIVEPREFIX=prefix
+S3CMD=s3cmd
+TMPDIR=/tmp
+
+ARCHIVEFILE=$ARCHIVEPREFIX$(date +"%m_%d_%Y_%H_%M").tar.gz
+
+tar zcvf $TMPDIR/$ARCHIVEFILE $BACKUP_SOURCE
+
+$S3CMD put --progress $TMPDIR/$ARCHIVEFILE s3://$S3BUCKET/$ARCHIVEFILE
+
+rm $TMPDIR/$ARCHIVEFILE
+
+#tar zcvf - $BACKUP_SOURCE | $S3CMD put --progress - s3://$S3BUCKET/$ARCHIVEFILE
